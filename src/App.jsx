e13675b1cce37c51921795da43e632e2987144c6
guru <uguruvishwa@gmail.com>
2024-01-02 22:03:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.scss"
+import Navbar from "./component/navbar/navbar";
+
+const App = () => {
+  return (
+    <div>
+      <section><Navbar>  </Navbar> </section>
+      <section>Profession </section>
+      <section>Business </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return <div className="navbar">
+  {/* SideBar */}
+  <div className="wrapper"> </div>
+  </div>;
+};
+
+export default Navbar;
