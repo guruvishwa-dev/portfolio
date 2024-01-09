6f3367a97f56e9bc4bfd77e844b536907d992241
guru <uguruvishwa@gmail.com>
2024-01-09 10:06:31 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Test from "./Test";
 import "./app.scss"
 import Navbar from "./component/navbar/navbar";
 
 const App = () => {
   return (
-    <div>
-      <section><Navbar>  </Navbar> </section>
-      <section>Profession </section>
-      <section>Business </section>
-    </div>
+    <><Test /><Test /></>
+    // <div>
+    //   <section> <Navbar/> </section>
+    //   <section>Profession </section>
+    //   <section>Business </section>
+    // </div>
   );
 };
 
--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,10 +1,26 @@
-import "./navbar.scss"
+import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">
-  {/* SideBar */}
-  <div className="wrapper"> </div>
-  </div>;
+  return (
+    <div className="navbar">
+      {/* SideBar */}
+      <div className="wrapper">
+        <span>Guruvishwa </span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt=" No image" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt=" No image" />
+          </a>
+          <a href="#">
+            <img src="/linkedin.png" alt=" No image" />
+          </a>
+        </div>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Navbar;
